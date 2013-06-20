6840109fb3d511a3e8c746f5c780a15f40afa72d
David M. Lee <dlee@digium.com>
2013-06-20 10:32:13 -0500

    Dist changes to load localhost Asterisk ARI by default

--- dist/swagger-ui.js
+++ dist/swagger-ui.js
@@ -1347,8 +1347,7 @@ templates['status_code'] = template(function (Handlebars,depth0,helpers,partials
     }
 
     HeaderView.prototype.events = {
-      'click #show-pet-store-icon': 'showPetStore',
-      'click #show-wordnik-dev-icon': 'showWordnikDev',
+      'click #show-asterisk-icon': 'showAsterisk',
       'click #explore': 'showCustom',
       'keyup #input_baseUrl': 'showCustomOnKeyup',
       'keyup #input_apiKey': 'showCustomOnKeyup'
@@ -1356,16 +1355,9 @@ templates['status_code'] = template(function (Handlebars,depth0,helpers,partials
 
     HeaderView.prototype.initialize = function() {};
 
-    HeaderView.prototype.showPetStore = function(e) {
+    HeaderView.prototype.showAsterisk = function(e) {
       return this.trigger('update-swagger-ui', {
-        discoveryUrl: "http://petstore.swagger.wordnik.com/api/api-docs.json",
-        apiKey: "special-key"
-      });
-    };
-
-    HeaderView.prototype.showWordnikDev = function(e) {
-      return this.trigger('update-swagger-ui', {
-        discoveryUrl: "http://api.wordnik.com/v4/resources.json",
+        discoveryUrl: "http://localhost:8088/ari/api-docs/resources.json",
         apiKey: ""
       });
     };
